49e720dcba7ff85b0f766986511dc5673e74cdae
Habib Sellami <habib.sellami@outlook.com>
2023-05-31 20:33:33 +0100

    Add login page

--- src/lib/axios.tsx
+++ src/lib/axios.tsx
@@ -1,8 +1,8 @@
 import {
-    AxiosError,
-    AxiosResponse,
-    // eslint-disable-next-line import/no-named-default
-    default as Axios, InternalAxiosRequestConfig,
+  AxiosError,
+  AxiosResponse,
+  // eslint-disable-next-line import/no-named-default
+  default as Axios, InternalAxiosRequestConfig,
 } from 'axios';
 import { ReactNode, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -12,50 +12,50 @@ import { useNotificationStore } from '@/stores/notifications';
 import { API_URL } from '@/config';
 
 const axios = Axios.create({
-    baseURL: API_URL,
+  baseURL: API_URL,
 });
 
 export function AxiosInterceptor({ children } : { children : ReactNode }) {
-    const [isSet, setIsSet] = useState(false);
-    const navigate = useNavigate();
-    const signOut = useSignOut();
-    // this function intercepts requests and makes sure that the token is added to the request header
-    const requestInterceptor = (config: InternalAxiosRequestConfig<any>) => {
-        const token = Cookies.get('_auth');
-        if (token) {
-            // eslint-disable-next-line no-param-reassign
-            config.headers!.authorization = `Bearer ${token}`;
-        }
-        // eslint-disable-next-line no-param-reassign
-        config.headers!.Accept = 'application/json';
-        return config;
-    };
-    const responseInterceptor = (response : AxiosResponse) => response.data;
-    const errorInterceptor = (error : AxiosError) => {
-        const message = error.response?.data || 'Something went wrong...';
-        useNotificationStore.getState().addNotification({
-            type: 'error',
-            title: 'Request failed',
-            message: message as string,
-        });
-        if (error.response?.status === 401) {
-            signOut();
-            navigate('/auth/login');
-        }
-        return Promise.reject(error);
-    };
+  const [isSet, setIsSet] = useState(false);
+  const navigate = useNavigate();
+  const signOut = useSignOut();
+  // this function intercepts requests and makes sure that the token is added to the request header
+  const requestInterceptor = (config: InternalAxiosRequestConfig<any>) => {
+    const token = Cookies.get('_auth');
+    if (token) {
+      // eslint-disable-next-line no-param-reassign
+      config.headers!.authorization = `Bearer ${token}`;
+    }
+    // eslint-disable-next-line no-param-reassign
+    config.headers!.Accept = 'application/json';
+    return config;
+  };
+  const responseInterceptor = (response : AxiosResponse) => response.data;
+  const errorInterceptor = (error : AxiosError) => {
+    const message = error.response?.data || 'Something went wrong...';
+    useNotificationStore.getState().addNotification({
+      type: 'error',
+      title: 'Request failed',
+      message: message as string,
+    });
+    if (error.response?.status === 401) {
+      signOut();
+      navigate('/auth/login');
+    }
+    return Promise.reject(error);
+  };
 
-    useEffect(() => {
-        axios.interceptors.request.use(requestInterceptor);
-        const interceptor = axios.interceptors.response.use(responseInterceptor, errorInterceptor);
-        setIsSet(true);
-        return () => axios.interceptors.response.eject(interceptor);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-    return (
-        <>
-            { isSet && children }
-        </>
-    );
+  useEffect(() => {
+    axios.interceptors.request.use(requestInterceptor);
+    const interceptor = axios.interceptors.response.use(responseInterceptor, errorInterceptor);
+    setIsSet(true);
+    return () => axios.interceptors.response.eject(interceptor);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return (
+    <>
+      { isSet && children }
+    </>
+  );
 }
 export default axios;
